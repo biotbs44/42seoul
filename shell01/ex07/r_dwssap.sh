3eb8e43027665aaa67f94c27ff77d703ac05a75e
biotbs44 <immchaebol@naver.com>
2021-09-18 21:48:32 +0900

    ex07

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat /etc/passwd | grep -v '#' | awk 'NR>=2 {print;}' | cut -f 1 -d ':' | rev | sort -f -r | awk 'NR>='${FT_LINE1}' && NR<='${FT_LINE2}' {print;}' | tr '\n' ',' | sed 's/,/, /g' | sed -e '{N;s/, /./}' 
+let "LINE = $FT_LINE2 - $FT_LINE1 + 1"
+
+cat /etc/passwd | grep -v '#' | awk 'NR>=2 {print;}' | cut -f 1 -d ':' | rev | sort -f -r | awk 'NR>='${FT_LINE1}' && NR<='${FT_LINE2}' {print;}' | sed "${LINE}s/\_/\_\_/" | tr '\n' ',' | sed 's/,/, /g' | sed 's/__,/./g' | sed 's/\.\ /./g'
+
+
