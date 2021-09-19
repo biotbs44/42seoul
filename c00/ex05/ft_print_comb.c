ae765f655cac32e1aea24c365f66ff943db89ea6
Junghun Yang <jungyang@c2r7s7.42seoul.kr>
2021-09-19 18:11:37 +0900

    ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
-int		ft_sort_count(int	num);
-void	ft_print_char(char	a, char	b, char c);
+void	ft_sort_num(int	num);
+void	ft_print_num(char	a, char	b, char c);
 
 void	ft_print_comb(void)
 {
-	int		count;
+	int		num;
 	char	a;
 	char	b;
 	char	c;
@@ -12,35 +12,49 @@ void	ft_print_comb(void)
 	a = 0;
 	b = 0;
 	c = 0;
-	count = 12;
-	while (count <= 789)
+	num = 12;
+	while (num <= 789)
 	{
-		c = (count % 10) + 48;
-		b = ((count / 10) % 10) + 48;
-		a = (((count / 10) / 10) % 10) + 48;
-		if ((a != b) && (b != c) && (c != a))
+		if (num == 789)
 		{
-			if ((a < b) && (b < c))
-			{
-				write(1, &a, 1);
-				write(1, &b, 1);
-				write(1, &c, 1);
-				if (count == 789)
-					break ;
-				write(1, ", ", 2);
-			}
+			write(1, "7", 1);
+			write(1, "8", 1);
+			write(1, "9", 1);
+			break ;
 		}
-		count++;
+		ft_sort_num(num);
+		num++;
 	}
 }
 
-void ft_sort_count(int	num)
+void	ft_sort_num(int	num)
 {
 	char	a;
 	char	b;
 	char	c;
 
-	a = 0;
-	b = 0;
-	c = 0;
+	c = (num % 10) + 48;
+	b = ((num / 10) % 10) + 48;
+	a = (((num / 10) / 10) % 10) + 48;
+	if ((a != b) && (b != c) && (a != c))
+	{
+		if ((a < b) && (b < c))
+		{
+			ft_print_num(a, b, c);
+		}
+	}
+}	
 
+void	ft_print_num(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	write(1, ", ", 2);
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
