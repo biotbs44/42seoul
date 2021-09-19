83c69a234727d3dd4ed3005a5caaa286beba7473
Junghun Yang <jungyang@c2r7s7.42seoul.kr>
2021-09-19 21:38:54 +0900

    ex08_2

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -1,43 +1,86 @@
 #include <unistd.h>
-
-void	ft_write_combn(char	*str, int	n);
-void	ft_plus_combn(char	*str, int	n);
+void	ft_print_combn_plus(char	*str, int	n);
+void	ft_print_combn_prior(char	*str, int	n);
+void	ft_print_combn_write(char	*str, int	n);
 
 void	ft_print_combn(int	n)
 {
 	char	str[10];
 	int		idx;
-	
+
 	idx = 0;
 	while (idx < 10)
 	{
 		if (idx >= n)
 			str[idx++] = '\0';
 		else
-			str[idx++] = 48;
-
+			str[idx++] = '0';
 	}
+	ft_print_combn_plus(str, n);
+}
+
+void	ft_print_combn_plus(char	*str, int	n)
+{
+	int	last_idx;
+	int	first_idx;
+
+	last_idx = n - 1;
+	first_idx = 0;
 	while (1)
 	{
-		while (n >= 0)
+		while (last_idx >= 0)
 		{
-			if(str[n - 1] != 9)
+			if (str[last_idx] != '9')
 			{
-				str[n - 1]++;
+				str[last_idx]++;
 				break ;
 			}
 			else
 			{
-				str[n - 1] = 0;
-				n--;
+				str[last_idx] = '0';
+				last_idx--;
 			}
 		}
+		ft_print_combn_prior(str, n);
+		if(str[0] == '9')
+			break ;
 	}
 }
 
-void	ft_plus_combn(char	*str, int	n)
+void	ft_print_combn_prior(char	*str, int	n)
 {
-	int	last_idx;
 	int	first_idx;
-	
-	
+	int	last_idx;
+	int	flag;
+
+	flag = 0;
+	first_idx = 0;
+	last_idx = n - 1;
+	while (first_idx < n - 1)
+	{
+		if (str[first_idx] > str[first_idx + 1])
+		{
+			flag = 1;
+			break ;
+		}
+	}
+	if (flag == 0)
+	{
+		ft_print_combn_write(str, n);
+	}
+}
+
+void	ft_print_combn_write(char	*str, int	n)
+{
+	int	first_idx;
+
+	first_idx = 0;
+	while (first_idx < n)
+	{
+		write(1, &str[first_idx++], 1);
+	}
+	if (str[0] == ((char) n) + 48)
+		write(1, "\n", 1);
+	else
+		write(1, ", ", 2);
+}
--- c00/ex08/main.c
+++ c00/ex08/main.c
@@ -1,10 +1,21 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int	main(void)
 {
-	char	a;
-	
-	a = 0;
-	write(1, &a, 1);
+	int	num;
+
+	num = 1234;
+
+	while (num <= 56789)
+	{
+		ft_print_combn(num);
+		num++;
+	}
 	return (0);
 }
+
+void	ft_print_combn(num)
+{
+	if(
+
