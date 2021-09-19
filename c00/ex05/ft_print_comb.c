53da4c6fc48d078ca84c93158eaba18ee13fa30f
Junghun Yang <jungyang@c2r7s7.42seoul.kr>
2021-09-19 17:05:47 +0900

    c00_ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,31 +1,46 @@
 #include <unistd.h>
+int		ft_sort_count(int	num);
+void	ft_print_char(char	a, char	b, char c);
 
 void	ft_print_comb(void)
 {
-	int	count;
+	int		count;
 	char	a;
 	char	b;
 	char	c;
 
+	a = 0;
+	b = 0;
+	c = 0;
 	count = 12;
 	while (count <= 789)
 	{
 		c = (count % 10) + 48;
 		b = ((count / 10) % 10) + 48;
 		a = (((count / 10) / 10) % 10) + 48;
-		
-		if((a != b) && (b != c) && (c != a))
+		if ((a != b) && (b != c) && (c != a))
 		{
-			if((a < b) && (b < c))
+			if ((a < b) && (b < c))
 			{
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if(count == 789)
-					break;
+				if (count == 789)
+					break ;
 				write(1, ", ", 2);
 			}
 		}
 		count++;
 	}
-}	
+}
+
+void ft_sort_count(int	num)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = 0;
+	b = 0;
+	c = 0;
+
